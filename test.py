51544f545accca11653cb37366dde8006e913fb6
Johnny <t.babel@gmx.com>
2019-07-17 13:45:08 +0200

    test

--- test.py
+++ test.py
@@ -1,37 +1,12 @@
-# imports ----------------------------------------------------------------------
-import time
-import random
-import speech_recognition as sr
-import eliza
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
 
-# classes ----------------------------------------------------------------------
-# child class of Eliza to make it possible to rewrite the run method
-class postbox(eliza.Eliza):
-    # the run-loop (conversation)
-    def run(self):
-        said = ''
-        repeat = True
-        # repeat until no error has occured and something has been recognized
-        while repeat:
-            with sr.Microphone() as source: # listen to microphone
-                audio = sr.listen(source)
-            try: # try to recognize
-                said = r.recognize_sphinx(audio)
-                print('> ' + said)
-                repeat = False
-            except sr.UnknownValueError: # handle errors. repeat
-                print('Speech Recognition could not understand audio')
-            except sr.RequestError as e: # handle errors. repeat
-                print('Could not request results from Speech Recognition program; {0}'.format(e))
-            if said == '': # nothing detected. repeat
-                print('No speech detected')
-
-        return True
-
-# main script ------------------------------------------------------------------
-posty = postbox()
-posty.load('postbox.txt')
-
-# main loop
-while True:
-    posty.run()
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
