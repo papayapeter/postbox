f520976dbd3e27ff8dd4d58f33865464bc33362b
Johnny <t.babel@gmx.com>
2019-07-18 21:50:42 +0200

    bash

--- postbox.sh
+++ postbox.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-python3 /home/pi/postbox/postbox.py
+python3 /home/pi/postbox/postbox.py &
+
+HALT=4
+
+while :
+do
+	# Check for halt button
+	if [ $(gpio -g read $HALT) -eq 0 ]; then
+		# Must be held for 2+ seconds before shutdown is run...
+		starttime=$(date +%s)
+		while [ $(gpio -g read $HALT) -eq 0 ]; do
+			if [ $(($(date +%s)-starttime)) -ge 10 ]; then
+				gpio -g write $LED 1
+				shutdown -h now
+			fi
+		done
+	fi
+done
